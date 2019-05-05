54c48e56cf8105541c9e3d34915c1b9e32eba312
Pedro D <josepedrodiaz@gmail.com>
2019-05-05 14:29:47 -0300

    El dimmer sube y baja

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const Gpio = require('pigpio').Gpio;
 const led = new Gpio(17, {mode: Gpio.OUTPUT});
  
 let dutyCycle = 0;
+
+let ascDecFlag = "ASC";// ASC pwm up DESC pwm down
+
+let stepSize;// tamanio del salto entre un pulso y otro
  
 setInterval(() => {
   led.pwmWrite(dutyCycle);
  
-  dutyCycle += 1;
+  if(ascDecFlag == "ASC"){
+    dutyCycle += stepSize;
+  }else{
+    dutyCycle -= stepSize;
+  }
+  
   if (dutyCycle > 255) {
-    dutyCycle = 0;
+    ascDecFlag == "DESC";
+  }else if (dutyCycle < 0){
+    ascDecFlag == "ASC";
   }
 }, 20);
