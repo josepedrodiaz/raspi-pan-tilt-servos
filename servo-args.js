50ba8e914ba569548de3b7e0ef0cdcbeee538e7a
Pedro D <josepedrodiaz@gmail.com>
2019-05-05 18:30:58 -0300

    paramters to positioning servos

---
+++ servo-args.js
@@ -0,0 +1,13 @@
+const Gpio = require('pigpio').Gpio;
+
+//read command line paramaters
+var h_v = process.argv.slice(2);
+var hPos = h_v[0];
+var vPos = h_v[1];
+ 
+const servoH = new Gpio(17, {mode: Gpio.OUTPUT});
+const servoV = new Gpio(27, {mode: Gpio.OUTPUT});
+
+servoH.servoWrite(hPos);
+servoV.servoWrite(servoV);
+
